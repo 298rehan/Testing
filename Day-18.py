d931627bc56f946ab0886b4554f04ff8dd908857
298rehan <rehanjameel298@gmail.com>
2026-08-20 22:07:15 +0500

    Day-18

---
+++ Day-18.py
@@ -0,0 +1,16 @@
+# numbers = int(input("Enter the numbers: "))
+
+# if numbers % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+num = int(input("Enter the numbers: "))
+
+if num >= 1:
+    print("Positive Number")
+elif num <= -1:
+    print("Negative Number")
+else:
+    print("Please Enter Greater or Lower digit from zero")
