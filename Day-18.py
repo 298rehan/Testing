81be38d8f7c1ffb7e1f0e20db998584d16d17fbc
298rehan <rehanjameel298@gmail.com>
2026-08-22 03:38:31 +0500

    Changes

--- Day-18.py
+++ Day-18.py
@@ -55,6 +55,70 @@
 # else:
 #     print("Invalid operator")
 
-for i in range(1, 101):
-    if i % 2 == 0:
-        print(i)
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         print(i)
+
+# number = 1 + 2 + 3 + 4 + 5 + 6
+# print(number)
+# num = int(input("Enter your number: "))
+# total = 0
+#
+# for i in range(1, num + 1):
+# total = total + i
+#
+# print("Sum is = ", total)
+#
+# table_number = int(input("Enter the number: "))
+#
+# for i in range(1, 11):
+# print(f"{table_number} * {i} = ", table_number * i)
+#
+
+
+# check_num = int(input("Enter the number: "))
+# even_count = 0
+# odd_count = 0
+# for i in range(1, check_num + 1):
+#     if i % 2 == 0:
+#         even_count = even_count + 1
+#     else:
+#         odd_count = odd_count + 1
+
+# print("Total Even Count = ", even_count)
+# print("Total Odd Count = ", odd_count)
+
+
+# Number Guessing Game
+
+# secret_number = 7
+
+# attempts = 0
+# while True:
+#     number = int(input("Enter the number b/w 1 to 10: "))
+#     if number > 10 or number <= 0:
+#         print("Please Enter number b/w 1 to 10")
+#     elif number >= 8:
+#         print("Go Lower!")
+#     elif number <= 6:
+#         print("Go Higher!")
+#     elif number == 7:
+#         print("You Guess Right!!!")
+#         break
+#     attempts += 1
+# print(f"You took {attempts + 1} attempts to guess the number.")
+# total attempts = 3 allow only 3 attempts to guess the password
+correct_password = "python123"
+
+attempts = 0
+while attempts < 3:
+    pass_checker = input(
+        "Guess the password: (Hint: Password starts with py or ends with 23): ")
+    if pass_checker == correct_password:
+        print("Access granted. Welcome!")
+        break
+    attempts += 1
+    if attempts < 3:
+        print("Incorrect password. Try again.")
+    else:
+        print("You have exceeded the maximum number of attempts. Access denied.")
