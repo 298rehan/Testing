420fb775e822cf599dcf5f69a52b4701acd25e5d
298rehan <rehanjameel298@gmail.com>
2026-09-09 04:43:15 +0500

    patterns

--- Day-18.py
+++ Day-18.py
@@ -250,3 +250,127 @@
 # print(f"Your Grade is: {Grade}")
 # status = "Pass" if percentage >= 40 else "Fail"
 # print(f"Your Result is: {status}")
+
+
+# start = int(input("Enter multiplication number = "))
+# end = int(input("Enter end number = "))
+
+# i = start
+# total = 0
+# while i <= end:
+# if i % 2 == 0 and i % 7 == 0:
+# total = total + i
+# i += 1
+# print(total)
+# i = 1
+# while i <= 10:
+# print(f"{start} * {i} = ", i * start)
+# i += 1
+# while i <= start:
+# if start % i == 0:
+# print(i)
+# i += 1
+# start = int(input("Enter start number = "))
+# end = int(input("Enter end number = "))
+#
+#
+# for i in range(1, start):
+# if start % i == 0:
+# print(i)
+#
+#
+# total = 0
+#
+# while True:
+# number = int(input("Enter your number: "))
+# if number < 0:
+# continue
+# if number == 0:
+# break
+# total += number
+#
+# print("The sum of the numbers is:", total)
+
+
+# for i in range(1, 8):
+#     for j in range(1, 6):
+#         print(j, end=" ")
+#     print()
+
+
+for i in range(5, 0, -1):
+    for j in range(6, 0, -1):
+        print(i, end=" ")
+    print()
+
+print()
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print("0", end=" ")
+    print()
+print()
+'''
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+'''
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+print()
+''' 
+1
+2 1
+3 2 1
+4 3 2 1 
+5 4 3 2 1
+'''
+for i in range(1, 6):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+
+print()
+
+'''
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+'''
+
+for i in range(5, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+print()
+'''
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+'''
+for i in range(1, 6):
+    for j in range(5, i-1, -1):
+        print(j, end=" ")
+    print()
+
+print()
+
+'''
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1 
+1
+'''
+for i in range(5, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
