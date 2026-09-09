f98cf56858040dc25cc696845ce8512e246e8b8f
298rehan <rehanjameel298@gmail.com>
2026-09-09 22:02:28 +0500

    Patterns

--- Day-18.py
+++ Day-18.py
@@ -374,3 +374,107 @@ for i in range(5, 0, -1):
     for j in range(i, 0, -1):
         print(j, end=" ")
     print()
+
+print()
+
+'''
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+'''
+
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+for i in range(4, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+print()
+"""
+5 
+5 4
+5 4 3
+5 4 3 2
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+"""
+
+for i in range(5, 0, -1):
+    for j in range(5, i-1, -1):
+        print(j, end=" ")
+    print()
+for i in range(1, 5):
+    for j in range(5, i, -1):
+        print(j, end=" ")
+    print()
+print()
+'''
+        1
+      1 2
+    1 2 3
+  1 2 3 4
+1 2 3 4 5
+'''
+
+for i in range(1, 6):
+    for k in range(1, 6 - i):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+print()
+
+'''
+        5
+      5 4
+    5 4 3
+  5 4 3 2
+5 4 3 2 1
+'''
+
+for i in range(1, 6):
+    for k in range(1, 6-i):
+        print(" ", end=" ")
+    for j in range(5, 5-i, -1):
+        print(j, end=" ")
+    print()
+
+print()
+
+'''
+       1
+     1 2 3 
+   1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+  1 2 3 4 5 6 7
+    1 2 3 4 5 
+      1 2 3
+        1
+'''
+
+for i in range(1, 6):
+    for j in range(1, 5 - i + 1):
+        print(" ", end=" ")
+    for k in range(1, (i * 2) - 1 + 1):
+        print("*", end=" ")
+    print()
+for i in range(4, 0, -1):
+    for j in range(1, 5 - i + 1):
+        print(" ", end=" ")
+    for k in range(1, (i * 2) - 1 + 1):
+        print("*", end=" ")
+    print()
+
+print()
